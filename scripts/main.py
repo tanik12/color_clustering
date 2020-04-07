1b394182f86ae9944f237fc298f9ecd1be40bb30
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-04-07 13:54:04 +0900

    [update]不要なコメントや処理の削除

--- scripts/main.py
+++ scripts/main.py
@@ -10,9 +10,7 @@ from utils.hist import centroid_histogram
 from utils.visualize import plot_colors
 
 def main(img):
-    N=7
-    sample_img=[i[int(w/10):int(N*w/10), int(h/10):int(N*h/10)] for i in img]
-    sample_img=[i.reshape(-1,3) for i in sample_img]
+    sample_img=[i.reshape(-1,3) for i in img]
     color=[]
     for i in sample_img:
         clt = KMeans(n_clusters = 3)
@@ -29,5 +27,3 @@ if __name__ == "__main__":
     img=np.reshape(img, (len(img),150,150,3)) 
     b,w,h,c=img.shape
     main(img)
-
-#    hstack=np.hstack(img)
